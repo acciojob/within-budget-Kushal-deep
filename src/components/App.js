d4eab5eabfbbcb31c130681fb77e96083065562c
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-26 15:39:52 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -21,19 +21,22 @@ const [budget,setBudget]=useState("")
         <h1>Enter your budget to check available items</h1>
         <input type="number" value={budget} onChange={(e)=>setBudget(e.target.value)}></input>
 
-        <h3>Items you can buy are in Green color</h3>
+        
 
         <table>
+          <thead>Items you can buy are in Green color</thead>
 
         {
           
           items.map((thing,index) => {
             const affordable = thing.price <=Number(budget)
             return(
+              <tbody>
                 <tr key={index} >
             <td>{thing.name}</td>
             <td style={{ color: affordable ? 'green' : 'red' }}>{thing.price}</td>
             </tr>
+            </tbody>
             )
           
 })
